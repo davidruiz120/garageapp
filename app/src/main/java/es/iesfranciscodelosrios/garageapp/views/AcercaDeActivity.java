009dcfbad008a37dbf34eda9facd31bfdfb17763
davidruiz120 <davidruiz.99@hotmail.com>
2019-11-20 18:43:48 +0100

    Añadido de nuevo AcercaDeActivity

--- app/src/main/java/es/iesfranciscodelosrios/garageapp/views/AcercaDeActivity.java
+++ app/src/main/java/es/iesfranciscodelosrios/garageapp/views/AcercaDeActivity.java
@@ -1,8 +1,14 @@
 package es.iesfranciscodelosrios.garageapp.views;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
 
 import es.iesfranciscodelosrios.garageapp.R;
 
@@ -12,5 +18,10 @@ public class AcercaDeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
     }
+
 }
