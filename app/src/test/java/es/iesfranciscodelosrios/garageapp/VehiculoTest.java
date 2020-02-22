96e37ab0c63a36a4ca31b5a30a01466e1a8ed598
davidruiz120 <36838431+davidruiz120@users.noreply.github.com>
2020-02-22 10:54:39 +0100

    Pruebas en getters

--- app/src/test/java/es/iesfranciscodelosrios/garageapp/VehiculoTest.java
+++ app/src/test/java/es/iesfranciscodelosrios/garageapp/VehiculoTest.java
@@ -1,5 +1,7 @@
 package es.iesfranciscodelosrios.garageapp;
 
+import android.text.TextUtils;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,6 +23,7 @@ public class VehiculoTest {
         assertEquals(false, vtest.setMarca(""));
         assertEquals(true, vtest.setMarca("Renault"));
         assertEquals(false, vtest.setMarca("Renaultttttttttttttttttttttttttt"));
+        assertEquals(false, vtest.getMarca().equals(""));
     }
 
     @Test
@@ -28,6 +31,7 @@ public class VehiculoTest {
         assertEquals(false, vtest.setModelo(""));
         assertEquals(true, vtest.setModelo("Megane"));
         assertEquals(false, vtest.setModelo("Meganeeeeeeeeeeeeeeeeeeeeeeeeeeeee"));
+        assertEquals(false, vtest.getModelo().equals(""));
     }
 
     @Test
@@ -36,6 +40,7 @@ public class VehiculoTest {
         assertEquals(true, vtest.setAnyo(2020));
         assertEquals(false, vtest.setAnyo(1492));
         assertEquals(false, vtest.setAnyo(2030));
+        assertEquals(2020, vtest.getAnyo());
     }
 
     @Test
@@ -43,14 +48,18 @@ public class VehiculoTest {
         assertEquals(false, vtest.setTraccion(""));
         assertEquals(true, vtest.setTraccion("Delantera"));
         assertEquals(false, vtest.setTraccion("Delanteraaaaaaaaaaaaaaaaaaaaaaaaa"));
+        assertEquals(false, vtest.getTraccion().equals(""));
     }
 
     @Test
     public void cpVehiculoFechaMatriculacion() {
         assertEquals(false, vtest.setFechamatriculacion(""));
         assertEquals(true, vtest.setFechamatriculacion("18/02/2020"));
+        assertEquals(false, vtest.setFechamatriculacion("18/15/2020"));
+        assertEquals(false, vtest.setFechamatriculacion("32/02/2020"));
         assertEquals(false, vtest.setFechamatriculacion("18/02"));
         assertEquals(false, vtest.setFechamatriculacion("18/02/202020"));
         assertEquals(false, vtest.setFechamatriculacion("2020"));
+        assertEquals(false, vtest.getFechamatriculacion().equals(""));
     }
 }
